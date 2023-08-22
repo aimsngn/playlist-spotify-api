f9475da5d9097e41267fe13f845b4d2791d1a8d9
aims <aimsvillano@gmail.com>
2023-08-22 15:29:14 -0700

    Fixed redirect and route paths

--- main.py
+++ main.py
@@ -10,27 +10,24 @@ import random
 # These are the client ids of the app for the Spotify API
 client_id = "0344808376044123b52d771e92858c83"
 client_secret = "c358e0a6e90d480cacbc745fddc15cf7"
-redirct_uri = "http://127.0.0.1:5000/"
+redirct_uri = "http://127.0.0.1:5000/redirected"
 
 app = Flask(__name__)
 app.secret_key = "oijdwaoidjwaiojdai"
 app.config['SESSION_COOKIE_NAME'] = 'COOKIE'
 
-@app.route('/')
-def index():
-    return "home"
-
-@app.route('/Tracks')
-def getTracks():
-    return "tracks"
-
-@app.route('/login')
-def login():
-    scope = "playlist-modify-public"
+@app.route('/') 
+def start_login():
+    scope = "playlist-modify-public playlist-modify-private"
     state = generate_random_string(16)
     auth_url = ('https://accounts.spotify.com/authorize?'f'response_type=code&client_id={client_id}&'f'scope={scope}&redirect_uri={redirct_uri}&'f'state={state}')
     
     return redirect(auth_url)
+
+@app.route('/redirected') #returns here
+def handle_redirect():
+    return "redirected"
+
 
 def generate_random_string(length):
     letters = string.ascii_letters + string.digits
