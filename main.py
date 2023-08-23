59a6bb636402cb63abcab981e80f64e910053d03
aims <aimsvillano@gmail.com>
2023-08-22 18:36:07 -0700

    Deployment server is working!
    It captures user information, creates a playlist under user account,
    and adds tracks based on artists.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import base64
 import requests
-from requests import post, get, post
+from requests import post, get
 import json
 from flask import Flask, request, url_for, session, redirect #will be used for redirect_uri; esssentially where our "app" is hosted
 import spotipy
@@ -27,16 +27,6 @@ def start_login():
     
     return redirect(auth_url)
 
-
-# swap the authorization code for the access token (since the user authorized us (gave us code)), we gon swap it for access token
-# access token will be used to create playlists and shit
-
-#Response
-#If the user accepts your request, then the user is redirected back to the application using the redirect_uri passed on the authorized request described above.
-#The callback contains two query parameters:
-#Query Parameter   |  	 Value
-#code	|    An authorization code that can be exchanged for an Access Token.
-#state	|    The value of the state parameter supplied in the request.
 
 @app.route('/redirected') #returns here
 def handle_redirect():
@@ -70,14 +60,95 @@ def handle_redirect():
             refresh_token = json_result["refresh_token"]
             session[TOKEN_INFO] = token
             session[REFRESH_INFO] = refresh_token
-            return redirect(url_for('getTracks', _external=True))
+            return redirect(url_for('createPlaylist', _external=True))
         
     return "token retrieve fails"
 
-@app.route('/getTracks')
+
+
+@app.route('/createPlaylist') 
+def createPlaylist():
+    token = get_token() #checks if expired or nah
+    header = get_auth_header(token)
+    
+    user_id = getUser()
+    data = {"name": "Your Mixed Playlist!!", "description": "enjoy your combined playlists <3", "public": True}
+ 
+    # creating the playlist
+    url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+    response = post(url, headers=header, json=data)
+    json_result = json.loads(response.content)
+    playlist_id = json_result["id"]
+    playlist_url = json_result["external_urls"]["spotify"]
+    
+    # adding the tracks to the playlist
+    response_code = addTracks(playlist_id)
+    
+    if response_code == 201:
+        return "success added tracks: " + playlist_url
+    
+    return redirect(url_for('start_login'), _external= True)
+    
+    
+
+def addTracks(playlist_id):
+    token = get_token() #checks if expired or nah
+    header = get_auth_header(token)
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+    tracks_uri = getTracks()
+    data = {"uris": tracks_uri,"position": 0}
+    response = post(url, headers=header, json=data)
+    
+    return response.status_code
+    
+    
+def getUser():
+    token = get_token() #checks if expired or nah
+    header = get_auth_header(token)
+    url = "https://api.spotify.com/v1/me"
+    
+    result = get(url, headers=header)
+    json_result = json.loads(result.content)
+    user_id = json_result["id"]
+    return user_id
+    
+
 def getTracks():
     token = get_token() #checks if expired or nah
-    return "token retrieved" + token
+    header = get_auth_header(token)
+    artist_ids = search_artist(header, artist_names=["Lola Amour", "Casey Lowry", "AJR"])
+    tracks = []
+    
+    for artist_id in artist_ids:
+        url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?market=US"
+        result = get(url, headers=header)
+        json_result = json.loads(result.content)["tracks"]
+        
+        for track in json_result:
+            tracks.append(track["uri"])
+        
+    return tracks
+
+
+def search_artist(header, artist_names):
+    artist_ids = []
+    url = "https://api.spotify.com/v1/search"
+    
+    for artist_name in artist_names:
+        query = f"q={artist_name}&type=artist&limit=1"
+        query_url = url + "?" + query
+    
+        result = get(query_url, headers=header)
+        json_result = json.loads(result.content)["artists"]["items"]
+        artist_ids.append(json_result[0]["id"])
+    
+    return artist_ids
+
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
 
 def get_token():
     token = session.get(TOKEN_INFO, None)
@@ -90,6 +161,7 @@ def get_token():
     return None
         #get token information for artist search and playlist creation
         #create a refresh token
+        
         
 def refresh_token():
     refresh_token = session.get(REFRESH_INFO, None)
@@ -120,9 +192,11 @@ def refresh_token():
             return None
     return None
         
+        
 def generate_random_string(length):
     letters = string.ascii_letters + string.digits
     return ''.join(random.choice(letters) for _ in range(length))
+    
     
 app.run()
 
